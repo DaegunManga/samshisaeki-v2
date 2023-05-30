a2898f9297668e6bdb8dc437be87efc828336f69
lukekeum <lukekeum11@gmail.com>
2023-05-30 10:18:02 +0900

    feat: setup backend project

---
+++ backend/types/config.d.ts
@@ -0,0 +1,28 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production' | 'test' | undefined;
+
+      PORT: string;
+      HOST: string;
+      CORS_WHITELISTS: string;
+      VERSION: string;
+
+      ACCESS_JWT_SECRET: string;
+      REFRESH_JWT_SECRET: string;
+      COOKIE_SECRET: string;
+      PROXY_ADDRESS: string;
+
+      // Typeorm Setting
+      TYPEORM_CONNECTION: 'postgres' | 'mysql' | 'sqlite' | 'mssql';
+      TYPEORM_HOST: string;
+      TYPEORM_PORT: string;
+      TYPEORM_USERNAME: string;
+      TYPEORM_PASSWORD: string;
+      TYPEORM_DATABASE: string;
+      TYPEORM_SYNCHRONIZE: 'true' | 'false';
+      TYPEORM_DROPSCHEMA: 'true' | 'false';
+      TYPEORM_LOGGING: 'true' | 'false';
+    }
+  }
+}
